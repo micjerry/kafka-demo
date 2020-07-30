9a16a6d0b4455eaba549607d879d4f3aaf3546b2
micjerry <hansfei@126.com>
2020-07-30 17:36:53 +0800

    initial

---
+++ KamaConsumer/kama-consumer-file/src/main/java/com/kama/file/KamaConsumer.java
@@ -0,0 +1,18 @@
+package com.kama.file;
+
+import java.io.IOException;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.kafka.annotation.KafkaListener;
+
+public class KamaConsumer {
+	private final Logger logger = LoggerFactory.getLogger(KamaConsumer.class);
+
+	
+	@KafkaListener(topics = "connect-kama-test")
+	public void receive(String message) throws IOException {
+		logger.info(String.format("#### -> Consumed message -> %s", message));
+	}
+
+}
---
+++ KamaConsumer/kama-consumer-file/src/main/java/com/kama/file/KamaConsumerApplication.java
@@ -0,0 +1,12 @@
+package com.kama.file;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KamaConsumerApplication {
+	public static void main(String[] args) {
+		SpringApplication.run(KamaConsumerApplication.class, args);
+	}
+
+}
